f119f485fa891668304355e6d3a9eb35638affc9
DiHNotAvailable <129920533+DiHNotAvailable@users.noreply.github.com>
2024-01-29 21:14:47 -0300

    carga inicial

---
+++ src/main/java/com/mycompany/inventarioprueba/InventarioPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventarioprueba;
+
+/**
+ *
+ * @author dhenr
+ */
+public class InventarioPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
